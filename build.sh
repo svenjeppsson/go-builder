2a7d6bfe45679f08ffb12fc05cec3ce3d0f6eb0e
Sven Jeppsson <sven@jeppsson.de>
2019-03-08 00:33:45 +0100

    initial

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+dep ensure
+go fmt ./...
+golangci-lint run
+go vet .
+go test -v ./... -run ^TestIntegration.* -coverprofile=cov1.out
+go test -v ./... -run ^TestUnit.* -coverprofile=cov2.out
+gocovmerge cov1.out cov2.out > cov.out
