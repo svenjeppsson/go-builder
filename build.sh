edcceba03f48aa9dcc7846ea3ad11bee915ce2c3
Sven Jeppsson <sven@jeppsson.de>
2019-03-08 17:49:41 +0100

    hopefully fixed coverageMege

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ dep ensure
 go fmt ./...
 golangci-lint run
 go vet .
-go test -v ./... -run ^TestIntegration.* -coverprofile=cov1.out
-go test -v ./... -run ^TestUnit.* -coverprofile=cov2.out
+go test -v ./... -run ^TestIntegration -coverprofile=cov1.out
+go test -v ./... -run ^TestUnit -coverprofile=cov2.out
 gocovmerge cov1.out cov2.out > cov.out
+cat cov.out
