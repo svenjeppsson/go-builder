dc52d51a180237e85cd7349c23e0dba8c28137ab
Sven Jeppsson <sven@jeppsson.de>
2019-03-09 08:04:59 +0100

    add chown to build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,5 @@ rm -rf mocks
 mockery -all -dir interfaces
 golangci-lint run
 go vet .
-go test -v ./... -run ^TestIntegration -coverprofile=cov1.out
-go test -v ./... -run ^TestUnit -coverprofile=cov2.out
-gocovmerge cov1.out cov2.out > cov.out
-rm cov1.out
-rm cov2.out
+go test -v ./... -run ^Test -coverprofile=cov.out
 chown -R $UID:$GID *
