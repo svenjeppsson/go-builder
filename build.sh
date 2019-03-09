6c5587d5802600045e3f638775621e9d0e0b98d7
Sven Jeppsson <sven@jeppsson.de>
2019-03-09 05:28:16 +0100

    rebuild mocks and rm coverages file

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 dep ensure
 go fmt ./...
+rm cov.out
+rm -rf mocks
+mockery -all -dir interfaces
 golangci-lint run
 go vet .
 go test -v ./... -run ^TestIntegration -coverprofile=cov1.out
 go test -v ./... -run ^TestUnit -coverprofile=cov2.out
 gocovmerge cov1.out cov2.out > cov.out
-cat cov.out
+rm cov1.out
+rm cov2.out
