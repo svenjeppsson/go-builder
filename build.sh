b3d510b4b4f90b2747d8fe194ab68353cfca0342
Sven Jeppsson <sven@jeppsson.de>
2019-03-11 02:06:23 +0100

    chown only when UID and GID are set

--- build.sh
+++ build.sh
@@ -6,5 +6,10 @@ rm -rf mocks
 mockery -all -dir interfaces
 golangci-lint run
 go vet .
-go test -v ./... -coverprofile=cov.out
-chown -R $UID:$GID *
+go test -v ./... -coverprofile=cov.out.tmp
+go test . -coverprofile cover.out.tmp
+cat cover.out.tmp | grep -v "mocks" > cover.out
+go tool cover -func cover.out
+if [ -n "$UID" -a -n "$GID" ]; then
+  chown -R $UID:$GID *
+fi
